59265943a15dd771ef5599823f055570152a84fe
Tùng Trần <tran.tung1311@gmail.com>
2025-04-20 18:32:43 +0700

    Create scripts folder Add js file

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,36 @@
+/**
+ * Check window width
+ * If pc view ==> run
+ */
+if (window.innerWidth >= 1024) {
+    window.addEventListener('DOMContentLoaded', () => {
+        /**
+         * Calculate width of images
+         * Set width caption follow width images
+         * @type {Element}
+         */
+        const imagesWrapper = document.querySelector('.layout__top-images')
+        const caption = document.querySelector('.layout__caption')
+        const images = imagesWrapper.querySelectorAll('.layout__image')
+        function setCaptionWidth() {
+            if (images.length === 2) {
+                const totalWidth = images[0].clientWidth + images[1].clientWidth + 16
+                caption.style.width = `${totalWidth}px`
+            }
+        }
+        setCaptionWidth()
+        window.addEventListener('resize', setCaptionWidth)
+
+        /**
+         * Check index element
+         * if even then add new class into element
+         * @type {NodeListOf<Element>}
+         */
+        const blocks = document.querySelectorAll('.layout__grid .layout__block');
+        blocks.forEach((block, index) => {
+            if ((index + 1) % 2 === 0) {
+                block.classList.add('reverse');
+            }
+        });
+    });
+}
